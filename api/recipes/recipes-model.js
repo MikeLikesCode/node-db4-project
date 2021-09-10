0d81f1195b348db048009499a5d2ce9e4f7fe594
Michael Guerrero <32268444+MikeLikesCode@users.noreply.github.com>
2021-09-10 10:59:39 -0400

    One tiny flaw

--- api/recipes/recipes-model.js
+++ api/recipes/recipes-model.js
@@ -32,13 +32,13 @@ async function getRecipeById(recipe_id) {
       "st.step_instructions",
       "ig.ingredient_name",
       "ig.ingredient_id",
-      "i.quantity"
+      "i.quantity",
+      "i.step_id as ingredient_step_id"
     )
     .leftJoin("steps as st", "r.recipe_id", "=", "st.recipe_id")
     .leftJoin("step_ingredients as i", "st.step_id", "=", "i.step_id")
     .leftJoin("ingredients as ig", "i.ingredient_id", "=", "ig.ingredient_id")
     .where("r.recipe_id", recipe_id)
-    .groupBy("st.step_number")
     .orderBy("st.step_number");
 
   const results = {
@@ -51,15 +51,21 @@ async function getRecipeById(recipe_id) {
             step_id: row.step_id,
             step_number: row.step_number,
             step_instructions: row.step_instructions,
-            ingredients: row.ingredient_id ? {
-                ingredient_id: row.ingredient_id,
-                ingredient_name: row.ingredient_name,
-                quantity: row.quantity
-              } : [],
+            ingredients: [],
           };
         })
       : [],
   };
+
+  rows.map((row,idx) => {
+    if (row.step_id == row.ingredient_step_id) {
+        results.steps[row.step_id - 1].ingredients.push({
+            ingredient_id: row.ingredient_id,
+            ingredient_name: row.ingredient_name,
+            quantity: row.quantity
+        })
+    }
+  });
 
   return results;
 }
